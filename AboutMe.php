b75833b96c02c5a54efd8bf5f9e2e5c7ab6201ee
muhammadfarhan08 <63396341+muhammadfarhan08@users.noreply.github.com>
2020-04-09 16:59:41 +0700

    Add files via upload

---
+++ AboutMe.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1> About Me </h1>";
+echo "Nama    : Muhammad Farhan";
+echo "<br>";
+echo "Sekolah : SMK Taruna Bhakti Depok";
+echo "<br>";
+echo "Kelas   : XI RPL 2";
+// echo "<br>";
+// echo "Alamar  : Jln. Sosial 3 Rw.021 Rt.08 No.16B";
+echo "<br>";
+echo "Nomer Hp : 085935304457 ";
+echo "<br>";
+echo "Email : mf08farhan@gmail.com"
